825b74a7865730b756c96808d4435650273f748d
Arijit Das <arijit10das2002@gmail.com>
2022-06-14 12:01:46 +0530

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,13 +1,40 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "first_name")
 	private String firstname;
+	
+	@Column(name = "last_name")
 	private String lastname;
+	
+	@Column(name = "email")
 	private String email;
 	
+	public User() {
+		
+	}
 	
+	
+	public User(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
 	public long getId() {
 		return id;
 	}
